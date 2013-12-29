92d64d97a4065d8b44407210b2e64b562910df2e
dead_horse <dead_horse@qq.com>
2013-12-30 01:55:41 +0800

    use koa-compose

--- servers/web.js
+++ servers/web.js
@@ -18,6 +18,8 @@ var koa = require('koa');
 var http = require('http');
 var logger = require('../common/logger');
 var rt = require('koa-rt');
+var compose = require('koa-compose');
+var favicon = require('koa-favicon');
 var session = require('../common/session');
 var config = require('../config');
 
@@ -28,15 +30,23 @@ app.outputErrors = config.debug;
 app.keys = config.cookieKeys;
 app.proxy = true; // to support `X-Forwarded-*` header
 
-app.use(rt());
-app.use(session);
+app.use(favicon());
 
-app.use(function *() {
+var middlewares = [];
+middlewares.push(rt());
+middlewares.push(session);
+
+
+middlewares.push(function *handler() {
   this.body = 'Hello NAE';
 });
 
+app.use(compose(middlewares));
+
 app.on('error', function (err) {
-  logger.error(err);
+  if (!err.status || err.status >= 500) {
+    logger.error(err);
+  }
 });
 
 module.exports = http.createServer(app.callback());
