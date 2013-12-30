30a91b8c489f5341762092c08cfc79878aa90896
dead_horse <dead_horse@qq.com>
2013-12-30 14:22:46 +0800

    remove koa-compose

--- servers/web.js
+++ servers/web.js
@@ -18,7 +18,6 @@ var koa = require('koa');
 var http = require('http');
 var logger = require('../common/logger');
 var rt = require('koa-rt');
-var compose = require('koa-compose');
 var favicon = require('koa-favicon');
 var session = require('../common/session');
 var config = require('../config');
@@ -31,17 +30,12 @@ app.keys = config.cookieKeys;
 app.proxy = true; // to support `X-Forwarded-*` header
 
 app.use(favicon());
+app.use(rt());
+app.use(session);
 
-var middlewares = [];
-middlewares.push(rt());
-middlewares.push(session);
-
-
-middlewares.push(function *handler() {
+app.use(function *handler() {
   this.body = 'Hello NAE';
 });
-
-app.use(compose(middlewares));
 
 app.on('error', function (err) {
   if (!err.status || err.status >= 500) {
